f69d75425173f84da848d6e90214a66d6d006c6c
Mamun-Udoy <abdullah15-2696@diu.edu.bd>
2025-02-06 12:37:05 +0600

    model, resource class, and repo created

---
+++ data/src/main/java/com/example/data/repository/AppRepository.kt
@@ -0,0 +1,12 @@
+package com.example.data.repository
+
+import com.example.network.api.ApiInterface
+import javax.inject.Inject
+
+class AppRepository @Inject constructor(
+    private val api: ApiInterface,
+) {
+    // fetch the data
+    suspend fun fetchRepo(query: String, sort: String, order: String, perPage: Int) =
+        api.searchRepositories(query = query, sort = sort, order = order, perPage = perPage)
+}
